b0124e0bab4f604faff11d427f77b31cd2e29d4e
Humberto Alfonso Díaz <humberto.alfonso@asvito.es>
2019-12-09 11:49:58 +0100

    FUNCT Add network api

--- src/network.js
+++ src/network.js
@@ -4,6 +4,14 @@ export function technologies() {
     return api_call("network-manager/technologies", {});
 }
 
+export function services() {
+    return api_call("network-manager/services", {});
+}
+
+export function scan_services(oOptions) {
+    return api_call("network-manager/scan_services", oOptions);
+}
+
 export function on_global_state(handler) {
     return api_subscribe("network-manager/subscribe", {
         value: "global_state"
