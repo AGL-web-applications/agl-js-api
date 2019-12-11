8e1b63d6ddaea0ad7453dcc0def957adfa9596dc
Humberto Alfonso Díaz <humberto.alfonso@asvito.es>
2019-12-11 10:59:38 +0100

    BUGFIX Correct connection host

--- src/api.js
+++ src/api.js
@@ -46,8 +46,8 @@ export function subscribe(url, event, handler) {
 
 export function init() {
     afb = new AFB({
-        host: "192.168.1.102:31031",
-        // host: host+":"+port,
+        // host: "192.168.1.102:31031",
+        host: host+":"+port,
         token: token
     });
 }
